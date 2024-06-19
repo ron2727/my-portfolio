a2e6e1554f0e2edf086dcb069b70ab9adfc0ef37
John Ron <kikobuere27@gmail.com>
2024-06-19 18:06:43 +0800

    Set links to all contact links icon

--- src/components/sections/Contact.vue
+++ src/components/sections/Contact.vue
@@ -6,29 +6,23 @@
                 Whether you have a project in mind, need assistance with web development, or simply want to discuss
                 ideas, I'd love to hear from you.</p>
             <div class="email-link-wrapper group relative">
-                <a href="mailto:1fLp7@example.com" class="email-link relative z-10 font-bold text-default text-sm md:text-lg dark:text-dark-default">kikobuere27@gmail.com</a>
+                <a href="mailto:kikobuere27@gmail.com" class="email-link relative z-10 font-bold text-default text-sm md:text-lg dark:text-dark-default">kikobuere27@gmail.com</a>
                 <div class=" absolute bottom-0 left-0 z-0 w-full h-0 transition-all duration-200 group-hover:h-1 bg-dark-secondary"></div>
             </div>
             <div class="wrapper flex space-x-2">
-                <a href="#">
+                <a href="https://github.com/ron2727" target="_blank">
                     <div
                         class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
                         <i class='bx bxl-github bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
                     </div>
-                </a>
-                <a href="#">
-                    <div
-                        class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
-                        <i class='bx bxs-envelope bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
-                    </div>
-                </a>
-                <a href="#">
+                </a> 
+                <a href="https://www.linkedin.com/in/john-ron-buere-9b1545191/" target="_blank">
                     <div
                         class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
                         <i class='bx bxl-linkedin bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
                     </div>
                 </a>
-                <a href="#">
+                <a href="https://web.facebook.com/profile.php?id=100010481264398" target="_blank">
                     <div
                         class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
                         <i class='bx bxl-facebook bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
--- src/components/sections/Intro.vue
+++ src/components/sections/Intro.vue
@@ -14,21 +14,21 @@
                         class=" inline-block px-4 border border-dark-secondary text-dark-secondary text-xs lg:text-sm text-center font-medium py-2 lg:py-3 transition-colors duration-200 hover:text-white hover:border-dark-secondary hover:bg-dark-secondary dark:text-dark-secondary dark:border-dark-secondary">My
                         Resume</a>
                     <div class="wrapper ml-10 flex space-x-2">
-                        <a href="#">
+                        <a href="https://github.com/ron2727" target="_blank">
                             <div
                                 class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
                                 <i
                                     class='bx bxl-github bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
                             </div>
                         </a>
-                        <a href="#">
+                        <a href="https://www.linkedin.com/in/john-ron-buere-9b1545191/" target="_blank">
                             <div
                                 class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
                                 <i
                                     class='bx bxl-linkedin bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
                             </div>
                         </a> 
-                        <a href="#">
+                        <a href="https://web.facebook.com/profile.php?id=100010481264398" target="_blank">
                             <div
                                 class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
                                 <i
