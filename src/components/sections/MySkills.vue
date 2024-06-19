83fcb2702fe068a1e6fc2f127c5f826a5d2dbb43
John Ron <kikobuere27@gmail.com>
2024-06-19 14:05:44 +0800

    Remove duplicate column php in skills section

--- src/components/sections/MySkills.vue
+++ src/components/sections/MySkills.vue
@@ -44,17 +44,7 @@
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">PHP</h6>
-            </div>
-             <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/php.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/php.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">PHP</h6>
-            </div>
+            </div> 
             <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
