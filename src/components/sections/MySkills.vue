696c9039104aaff793b64999e7d866c8748ae749
John Ron <kikobuere27@gmail.com>
2024-06-19 14:04:26 +0800

    Fixed links image of skills in skills section

--- src/components/sections/MySkills.vue
+++ src/components/sections/MySkills.vue
@@ -9,9 +9,9 @@
             <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/html.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/html.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/html.png" alt="logo">
+                     <img src="../../assets/image/skills/html.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">HTML</h6>
@@ -19,18 +19,18 @@
              <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/css.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/css.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/css.png" alt="logo">
+                     <img src="../../assets/image/skills/css.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">CSS</h6>
             </div>
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/js.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/js.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/js.png" alt="logo">
+                     <img src="../../assets/image/skills/js.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">JavaScript</h6>
@@ -38,9 +38,9 @@
              <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/php.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/php.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/php.png" alt="logo">
+                     <img src="../../assets/image/skills/php.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">PHP</h6>
@@ -48,9 +48,9 @@
              <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/php.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/php.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/php.png" alt="logo">
+                     <img src="../../assets/image/skills/php.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">PHP</h6>
@@ -58,9 +58,9 @@
             <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/mysql.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/mysql.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/mysql.png" alt="logo">
+                     <img src="../../assets/image/skills/mysql.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">MySQL</h6>
@@ -68,9 +68,9 @@
             <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/jquery.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/jquery.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/jquery.png" alt="logo">
+                     <img src="../../assets/image/skills/jquery.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">JQuery</h6>
@@ -78,9 +78,9 @@
             <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/bootstrap.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/bootstrap.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/bootstrap.png" alt="logo">
+                     <img src="../../assets/image/skills/bootstrap.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">Bootstrap</h6>
@@ -88,9 +88,9 @@
             <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/tailwind.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/tailwind.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/tailwind.png" alt="logo">
+                     <img src="../../assets/image/skills/tailwind.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">Tailwind</h6>
@@ -98,9 +98,9 @@
             <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/laravel.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/laravel.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/laravel.png" alt="logo">
+                     <img src="../../assets/image/skills/laravel.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">Laravel</h6>
@@ -108,9 +108,9 @@
             <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/vue.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/vue.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/vue.png" alt="logo">
+                     <img src="../../assets/image/skills/vue.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">Vue.js</h6>
@@ -118,9 +118,9 @@
             <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/csharp.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/csharp.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/csharp.png" alt="logo">
+                     <img src="../../assets/image/skills/csharp.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">C#</h6>
@@ -128,9 +128,9 @@
             <!-- Column -->
             <div class="col group relative flex flex-col items-center justify-center p-4">
                <div class="wrapper group relative">
-                  <img src="../assets/image/skills/git.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+                  <img src="../../assets/image/skills/git.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
                   <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../assets/image/skills/git.png" alt="logo">
+                     <img src="../../assets/image/skills/git.png" alt="logo">
                   </div> 
                </div>
                <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">Git</h6>
