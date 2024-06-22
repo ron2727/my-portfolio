2d8e730df1efb6e94e1a7045bc68dda6a9cf9124
John Ron <kikobuere27@gmail.com>
2024-06-23 07:35:07 +0800

    Add bluish on photos in about me

--- src/components/ui/CollectionPhotos.vue
+++ src/components/ui/CollectionPhotos.vue
@@ -2,14 +2,14 @@
     <div class=" wrapper relative group w-52 h-52 sm:w-64 sm:h-64 lg:w-80 lg:h-80">
       <div class="group relative z-40 w-full h-full">
         <img id="middle" src="../../assets/image/BUERE_JOHNRON1.png" class=" relative z-0 w-full h-full object-cover grayscale"/>
-        <div class=" absolute top-0 left-0 w-full h-full bg-dark-secondary/5"></div>
+        <div class=" absolute top-0 left-0 w-full h-full bg-dark-secondary/20"></div>
         <div class="absolute top-0 left-0 z-10 w-full h-0 overflow-hidden transition-all duration-300 ease-in-out group-hover:h-full">
             <img id="middle" src="../../assets/image/BUERE_JOHNRON1.png" class=" w-52 h-52 sm:w-64 sm:h-64 lg:w-80 lg:h-80 object-cover"/>
         </div>
       </div>
-      <div class=" absolute top-0 left-0 z-30 w-full h-full bg-default/40 transition-transform duration-300 delay-200 group-hover:translate-x-2 group-hover:translate-y-2 dark:bg-dark-secondary/40"></div>
-      <div class=" absolute top-0 left-0 z-20 w-full h-full bg-default/30 transition-transform duration-300 delay-100 group-hover:translate-x-4 group-hover:-translate-y-2 dark:bg-dark-secondary/30"></div>
-      <div class=" absolute top-0 left-0 z-0 w-full h-full bg-default/20 transition-transform duration-300 group-hover:translate-x-6 group-hover:translate-y-4 dark:bg-dark-secondary/20"></div>
+      <div class=" absolute top-0 left-0 z-30 w-full h-full transition-transform duration-300 delay-200 group-hover:translate-x-2 group-hover:translate-y-2 bg-dark-secondary/40"></div>
+      <div class=" absolute top-0 left-0 z-20 w-full h-full transition-transform duration-300 delay-100 group-hover:translate-x-4 group-hover:-translate-y-2 bg-dark-secondary/30"></div>
+      <div class=" absolute top-0 left-0 z-0 w-full h-full transition-transform duration-300 group-hover:translate-x-6 group-hover:translate-y-4 bg-dark-secondary/20"></div>
     </div>
 </template> 
  
