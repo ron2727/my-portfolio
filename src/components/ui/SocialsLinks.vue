3004c9b26dbd95dd167d4a7891f8ff8175d3216b
John Ron <kikobuere27@gmail.com>
2024-06-22 12:15:49 +0800

    created the SocialLinks component for github and linkedin links

---
+++ src/components/ui/SocialsLinks.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="wrapper flex space-x-2">
+        <a href="https://github.com/ron2727" target="_blank">
+            <div
+                class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
+                <i class='bx bxl-github bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
+            </div>
+        </a>
+        <a href="https://www.linkedin.com/in/john-ron-buere-9b1545191/" target="_blank">
+            <div
+                class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
+                <i class='bx bxl-linkedin bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
+            </div>
+        </a>
+        <a href="https://web.facebook.com/profile.php?id=100010481264398" target="_blank">
+            <div
+                class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
+                <i class='bx bxl-facebook bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
+            </div>
+        </a>
+    </div>
+</template>
