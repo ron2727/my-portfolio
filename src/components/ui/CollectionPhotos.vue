c06347b6482ea3c578c099ca98c08d98c14973aa
John Ron <kikobuere27@gmail.com>
2024-06-23 11:44:34 +0800

    Change hover design of image in about me

--- src/components/ui/CollectionPhotos.vue
+++ src/components/ui/CollectionPhotos.vue
@@ -1,10 +1,10 @@
 <template>
     <div class=" wrapper relative group w-52 h-52 sm:w-64 sm:h-64 lg:w-80 lg:h-80">
       <div class="group relative z-40 w-full h-full">
-        <img id="middle" src="../../assets/image/BUERE_JOHNRON1.png" class=" relative z-0 w-full h-full object-cover grayscale"/>
+        <img id="middle" src="../../assets/image/me2.png" class=" relative z-0 w-full h-full object-cover grayscale"/>
         <div class=" absolute top-0 left-0 w-full h-full bg-dark-secondary/20"></div>
         <div class="absolute top-0 left-0 z-10 w-full h-0 overflow-hidden transition-all duration-300 ease-in-out group-hover:h-full">
-            <img id="middle" src="../../assets/image/BUERE_JOHNRON1.png" class=" w-52 h-52 sm:w-64 sm:h-64 lg:w-80 lg:h-80 object-cover"/>
+            <img id="middle" src="../../assets/image/me1.png" class=" w-52 h-52 sm:w-64 sm:h-64 lg:w-80 lg:h-80 object-cover"/>
         </div>
       </div>
       <div class=" absolute top-0 left-0 z-30 w-full h-full transition-transform duration-300 delay-200 group-hover:translate-x-2 group-hover:translate-y-2 bg-dark-secondary/40"></div>
