968d3cea2cfd936e3d6b97db19f5f3cd16eafe8e
John Ron <kikobuere27@gmail.com>
2024-06-27 11:45:10 +0800

    Update link of linkedIn

--- src/components/ui/SocialsLinks.vue
+++ src/components/ui/SocialsLinks.vue
@@ -6,7 +6,7 @@
                 <i class='bx bxl-github bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
             </div>
         </a>
-        <a href="https://www.linkedin.com/in/john-ron-buere-9b1545191/" target="_blank">
+        <a href="https://www.linkedin.com/in/john-ron-buere-b27242316/" target="_blank">
             <div
                 class=" group bg-default pb-1 pt-2 px-2.5 border border-default rounded-full transition-all duration-200 hover:border-dark-secondary hover:scale-110 dark:bg-dark-body hover:bg-dark-secondary dark:border-dark-secondary">
                 <i class='bx bxl-linkedin bx-xs text-white group-hover:text-white dark:text-dark-secondary'></i>
