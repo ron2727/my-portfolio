8c2b79c144a1255dddd19551ebbf23d53f074a0f
John Ron <kikobuere27@gmail.com>
2024-07-08 09:07:16 +0800

    Create Skill component

--- src/components/sections/MySkills.vue
+++ src/components/sections/MySkills.vue
@@ -6,125 +6,31 @@
              These are technologies I've been working with.
          </h6>
          <div class="skills grid grid-cols-3 lg:grid-cols-6 gap-x-5 lg:gap-y-5 mt-5">
-            <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/html.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/html.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">HTML</h6>
-            </div>
+            <!-- Column --> 
+            <Skill image="html.png">HTML</Skill>
              <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/css.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/css.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">CSS</h6>
-            </div>
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/js.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/js.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">JavaScript</h6>
-            </div>
+            <Skill image="css.png">CSS</Skill>
+            <!-- Column -->
+            <Skill image="js.png">JavaScript</Skill>
+            <!-- Column -->
+            <Skill image="php.png">PHP</Skill>
              <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/php.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/php.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">PHP</h6>
-            </div> 
+            <Skill image="mysql.png">MySQL</Skill> 
             <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/mysql.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/mysql.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">MySQL</h6>
-            </div> 
+            <Skill image="jquery.png">JQuery</Skill> 
             <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/jquery.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/jquery.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">JQuery</h6>
-            </div>
+            <Skill image="bootstrap.png">Bootstrap</Skill> 
+            <!-- Column --> 
+            <Skill image="tailwind.png">Tailwind</Skill>
             <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/bootstrap.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/bootstrap.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">Bootstrap</h6>
-            </div>
+            <Skill image="laravel.png">Laravel</Skill>
             <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/tailwind.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/tailwind.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">Tailwind</h6>
-            </div>
+            <Skill image="vue.png">Vue.js</Skill>
             <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/laravel.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/laravel.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">Laravel</h6>
-            </div>
+            <Skill image="csharp.png">C#</Skill>
             <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/vue.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/vue.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">Vue.js</h6>
-            </div>
-            <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/csharp.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/csharp.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">C#</h6>
-            </div>
-            <!-- Column -->
-            <div class="col group relative flex flex-col items-center justify-center p-4">
-               <div class="wrapper group relative">
-                  <img src="../../assets/image/skills/git.png" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
-                  <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                     <img src="../../assets/image/skills/git.png" alt="logo">
-                  </div> 
-               </div>
-               <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary">Git</h6>
-            </div>
+            <Skill image="git.png">Git</Skill>
+            <!-- Column --> 
          </div>
       </template>
    </SectionLayout>
@@ -132,5 +38,6 @@
 
 <script setup>
 import SectionLayout from '../layouts/SectionLayout.vue'; 
+import Skill from '../ui/Skill.vue';
 </script>
  
