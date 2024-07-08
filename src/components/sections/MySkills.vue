02e4276fbaf3c8c07b6f449056df94d943ac0600
John Ron <kikobuere27@gmail.com>
2024-07-08 15:51:33 +0800

    Create Skill component

--- src/components/sections/MySkills.vue
+++ src/components/sections/MySkills.vue
@@ -5,32 +5,19 @@
           <h6 class="mt-5 text-sm md:text-lg font-medium text-default dark:text-dark-default">
              These are technologies I've been working with.
          </h6>
-         <div class="skills grid grid-cols-3 lg:grid-cols-6 gap-x-5 lg:gap-y-5 mt-5">
-            <!-- Column --> 
-            <Skill image="html.png">HTML</Skill>
-             <!-- Column -->
+         <div class="skills grid grid-cols-3 lg:grid-cols-6 gap-x-5 lg:gap-y-5 mt-5"> 
+            <Skill image="html.png">HTML</Skill> 
             <Skill image="css.png">CSS</Skill>
-            <!-- Column -->
             <Skill image="js.png">JavaScript</Skill>
-            <!-- Column -->
-            <Skill image="php.png">PHP</Skill>
-             <!-- Column -->
+            <Skill image="php.png">PHP</Skill> 
             <Skill image="mysql.png">MySQL</Skill> 
-            <!-- Column -->
             <Skill image="jquery.png">JQuery</Skill> 
-            <!-- Column -->
-            <Skill image="bootstrap.png">Bootstrap</Skill> 
-            <!-- Column --> 
+            <Skill image="bootstrap.png">Bootstrap</Skill>  
             <Skill image="tailwind.png">Tailwind</Skill>
-            <!-- Column -->
             <Skill image="laravel.png">Laravel</Skill>
-            <!-- Column -->
             <Skill image="vue.png">Vue.js</Skill>
-            <!-- Column -->
             <Skill image="csharp.png">C#</Skill>
-            <!-- Column -->
-            <Skill image="git.png">Git</Skill>
-            <!-- Column --> 
+            <Skill image="git.png">Git</Skill> 
          </div>
       </template>
    </SectionLayout>
--- src/components/ui/Skill.vue
+++ src/components/ui/Skill.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="col group relative flex flex-col items-center justify-center p-4">
         <div class="wrapper group relative">
-            <img ref="image1" alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
+            <img :src="imgSrc()"  alt="logo" class="w-20 lg:w-24 relative z-0 grayscale">
             <div class=" overflow-hidden absolute top-0 left-0 z-10 h-0 transition-all duration-500 group-hover:h-full">
-                <img ref="image2" alt="logo">
+                <img :src="imgSrc()" alt="logo">
             </div>
         </div>
         <h6 class=" w-min mt-3 lg:mt-2 font-medium text-default text-sm sm:text-lg opacity-0 transition-opacity duration-500 group-hover:opacity-100 dark:text-dark-primary"><slot></slot></h6>
@@ -11,16 +11,8 @@
 </template>
 
 
-<script setup>
-import { onMounted, ref } from 'vue';
-const props = defineProps(['image'])
+<script setup> 
+const props = defineProps(['image']) 
 
-const image1 = ref(null)
-const image2 = ref(null)
-
-onMounted(() => {
-    const imagePath = `${window.location.href}/src/assets/image/skills/${props.image}`;
-    image1.value.src = imagePath 
-    image2.value.src = imagePath 
-})
+const imgSrc = () => `/images/skills/${props.image}`;
 </script>
