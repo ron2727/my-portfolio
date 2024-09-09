cced3ad79143df73a5bd4390adc2f97bbbde7e18
John Ron <kikobuere27@gmail.com>
2024-09-09 08:32:44 +0800

    Change the text color of name in intro section

--- src/components/sections/Intro.vue
+++ src/components/sections/Intro.vue
@@ -2,10 +2,10 @@
     <section id="intro">
         <div class="main-wrapper max-w-5xl lg:h-screen m-auto flex flex-col justify-center md:mb-40 py-20 px-6 md:px-10 xl:px-0">
             <div class="main-wrapper space-y-4 lg:space-y-7 text-default dark:text-dark-primary">
-                <h1 class=" font-montserratAlternates mt-32 lg:mt-0 text-lg font-medium text-dark-secondary">Hi, my name is</h1>
+                <h1 class=" font-montserratAlternates mt-32 lg:mt-0 text-lg font-medium text-default/80 dark:text-dark-secondary">Hi, my name is</h1>
                 <div class=" wrapper text-4xl sm:text-5xl md:text-6xl lg:text-7xl ">
-                    <h1 class="font-bold text-default dark:text-dark-primary">John Ron Buere. </h1>
-                    <h1 class="font-bold text-default/80 dark:text-dark-primary/80">Software Developer.</h1>
+                    <h1 class="font-bold text-dark-secondary dark:text-dark-primary">John Ron Buere. </h1>
+                    <h1 class="font-bold text-default dark:text-dark-primary/80">Software Developer.</h1>
                 </div>
                 <h1 class=" max-w-2xl leading-normal text-sm sm:text-lg">I'm a recent graduate with a Bachelor of Science in
                     Information System. With a passion for both front-end and back-end development, I thrive on building dynamic, functional and user-friendly web</h1>
