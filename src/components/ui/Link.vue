9b9e53f5d27ca9a3611380acb6624d7ce655b810
John Ron <kikobuere27@gmail.com>
2024-09-21 10:43:06 +0800

    Add target blank attribute on component Link

--- src/components/ui/Link.vue
+++ src/components/ui/Link.vue
@@ -3,6 +3,7 @@
         <span class=" text-sm md:text-lg font-semibold text-default dark:text-dark-default">{{ label }}:</span>
         <div class="wrapper group relative">
             <a :href="link"
+                target="_blank"
                 class="link relative z-10 text-default text-sm md:text-lg dark:text-dark-default">{{ value }}</a>
             <div
                 class=" absolute bottom-0 left-0 z-0 w-full h-0 transition-all duration-200 group-hover:h-1 bg-dark-secondary">
